6133e0c6348d6cfa0ce999fabf35d2718da446c4
muroi <test.muroi@gmail.com>
2011-11-26 01:38:22 +0900

    Handle touch events in the touch pad view.

--- src/jp/gr/java_conf/remota/android/TouchPadActivity.java
+++ src/jp/gr/java_conf/remota/android/TouchPadActivity.java
@@ -1,20 +1,32 @@
 package jp.gr.java_conf.remota.android;
 
 import android.app.Activity;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 
 /**
- * This Activity appears as a dialog. It lists any paired devices 
- * When a device is chosen by the user, the MAC address of the device 
- * is sent back to the parent Activity in the result Intent.
+ * 
  */
-public class TouchPadActivity extends Activity {
+public class TouchPadActivity extends Activity implements View.OnTouchListener {
 	// Debugging
 	private static final String TAG = "TouchPadActivity";
 	private static final boolean DBG = true;
+	
+	// Constants
+	private static final int NOT_PRESSED = -1;
+	
+	// Member fields
+	private TouchPadView mTouchPadView;
+	
+	private int mLeftButtonPressed     = NOT_PRESSED;
+	private int mRightButtonPressed    = NOT_PRESSED;
+	private int mScrollBarPressed      = NOT_PRESSED;
+	private int mKeyboardButtonPressed = NOT_PRESSED;
+	private int mTouchPadPressed       = NOT_PRESSED;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +37,14 @@ public class TouchPadActivity extends Activity {
 		// To full screen
 		//getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+		mTouchPadView = new TouchPadView(this);
+		
+		// Set up to receive touch events
+		mTouchPadView.setOnTouchListener(this);
+		
 		// Set up the window layout
-		TouchPadView touchPadView = new TouchPadView(this);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(touchPadView);
+		setContentView(mTouchPadView);
 		
 		setResult(Activity.RESULT_OK);
 	}
@@ -38,4 +54,169 @@ public class TouchPadActivity extends Activity {
         
 		if(DBG) Log.i(TAG, "+++ ON DESTROY +++");
 	}
+	
+	/**
+	 * Called when the view is touched.
+	 */
+	public boolean onTouch(View view, MotionEvent event) {
+		if (view == mTouchPadView) {
+			if (DBG) Log.d(TAG, "Pointer count:" + event.getPointerCount());
+
+			RemotaService service = RemotaService.getInstance();
+			int c = event.getPointerCount();
+			float x, y;
+			int id, action, actionMasked, actionId;
+			String str = "";
+		
+			// the action information
+			action = event.getAction();
+		
+			// the action information without the pointer index
+			actionMasked = event.getActionMasked();
+		
+			// the index of the pointer that has gone down or up
+			actionId = event.getActionIndex();
+
+			// For debugging
+			if (DBG) {
+				Log.d(TAG, 
+						", action:" + action +
+						", actionMasked:" + actionMasked +
+						", actionId:" + actionId +
+						", history:" + event.getHistorySize()
+				);
+			}
+		
+			x = event.getX(actionId);
+			y = event.getY(actionId);
+			id = event.getPointerId(actionId);
+			PointF point = new PointF(x, y);
+
+			// Check whether a button is down or up.
+			if (actionMasked == MotionEvent.ACTION_DOWN ||
+					actionMasked == MotionEvent.ACTION_POINTER_1_DOWN ||
+					actionMasked == MotionEvent.ACTION_POINTER_DOWN) {
+				if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getLeftButtonRectF())) {
+					if (mLeftButtonPressed == NOT_PRESSED) {
+						mLeftButtonPressed = id;
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_LEFT_DOWN, (int)x, (int)y)
+						);
+					}
+				} else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getRightButtonRectF())) {
+					if (mRightButtonPressed == NOT_PRESSED) {
+						mRightButtonPressed = id;
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_RIGHT_DOWN, (int)x, (int)y)
+						);
+					}
+				} else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getScrollBarRectF())) { 
+					if (mScrollBarPressed == NOT_PRESSED) {
+						mScrollBarPressed = id;
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_WHELL, (int)x, (int)y)
+						);
+					}
+				} else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getKeyboardButtonRectF())) { 
+					if (mKeyboardButtonPressed == NOT_PRESSED) {
+						mKeyboardButtonPressed = id;
+					}
+				} else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getTouchPadRectF())) {
+					if (mTouchPadPressed == NOT_PRESSED) {
+						mTouchPadPressed = id;
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_MOVE, (int)x, (int)y)
+						);
+					}
+				}
+			} else if (actionMasked == MotionEvent.ACTION_UP ||
+					actionMasked == MotionEvent.ACTION_POINTER_1_UP ||
+					actionMasked == MotionEvent.ACTION_POINTER_UP) {
+				if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getLeftButtonRectF())) {
+					if (mLeftButtonPressed == id) {
+						mLeftButtonPressed = NOT_PRESSED;
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_LEFT_UP, (int)x, (int)y)
+						);
+					}
+				} else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getRightButtonRectF())) {
+					if (mRightButtonPressed == id) {
+						mRightButtonPressed = NOT_PRESSED;
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_RIGHT_UP, (int)x, (int)y)
+						);
+					}
+				} else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getScrollBarRectF())) { 
+					if (mScrollBarPressed == id) {
+						mScrollBarPressed = NOT_PRESSED;
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_WHELL, (int)x, (int)y)
+						);
+					}
+				} else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getKeyboardButtonRectF())) { 
+					if (mKeyboardButtonPressed == id) {
+						mKeyboardButtonPressed = NOT_PRESSED;
+					}
+				} else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getTouchPadRectF())) {
+					if (mTouchPadPressed == id) {
+						mTouchPadPressed = NOT_PRESSED;
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_MOVE, (int)x, (int)y)
+						);
+					}
+				}
+			} else if (actionMasked == MotionEvent.ACTION_MOVE) {
+				if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getTouchPadRectF())) {
+					if (mTouchPadPressed == id) {
+						service.sendMouseEvent(
+								new MouseEvent(MouseEvent.FLAG_MOVE, (int)x, (int)y)
+						);
+					}
+				}
+			}
+		
+			// For debugging
+			float hx, hy;
+			for (int i = 0; i < c; i++){
+				x = event.getX(i);
+				y = event.getY(i);
+				id = event.getPointerId(i);
+				point = new PointF(x, y);
+			
+				if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getLeftButtonRectF())) {
+					str = "left";
+				}
+				else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getRightButtonRectF())) {
+					str = "right";
+				}
+				else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getScrollBarRectF())) {
+					str = "scroll";
+				}
+				else if (TouchPadView.pointFIsInRectF(point, mTouchPadView.getKeyboardButtonRectF())) {
+					str = "keyboard";
+				}
+				
+				if (DBG) {
+					Log.d(TAG, 
+							"X" + i + ":" + x +
+							",Y" + i + ":" + y +
+							", id:" + id +
+							"," + str);
+				}
+				
+				if (DBG) {
+					if (event.getHistorySize() >= 1) {
+						hx = event.getHistoricalX(i, 1);
+						hy = event.getHistoricalY(i, 1);
+						Log.d(TAG, 
+								"HX" + i + 1 + ":" + hx +
+								"HY" + i + 1 + ":" + hy);
+					}
+				}
+			}
+		}
+		
+		return true;
+	}
 }
+
--- src/jp/gr/java_conf/remota/android/TouchPadView.java
+++ src/jp/gr/java_conf/remota/android/TouchPadView.java
@@ -1,15 +1,11 @@
 package jp.gr.java_conf.remota.android;
 
-import java.util.ArrayList;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.graphics.RectF;
-import android.graphics.Shader.TileMode;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -19,7 +15,7 @@ import android.view.View;
 /** 
  * This is the SurfaceView like a touch pad.
  */
-public class TouchPadView extends SurfaceView implements View.OnTouchListener, SurfaceHolder.Callback {
+public class TouchPadView extends SurfaceView implements SurfaceHolder.Callback {
 	// Debugging
 	private static final String TAG = "TouchPadView";
 	private static final boolean DBG = true;
@@ -28,17 +24,11 @@ public class TouchPadView extends SurfaceView implements View.OnTouchListener, S
 	private static final float BUTTON_HEIGHT_RATIO = 0.2f;
 	private static final float SCROLLBAR_WIDTH_RATIO = 0.08f;
 	private static final float KEYBOARD_BUTTON_HEIGHT_RATIO = 0.05f;
-	private static final int NOT_PRESSED = -1;
 	
 	// Member fields
 	private float mCanvasHeight = 0.0f;
 	private float mCanvasWidth  = 0.0f;
-	
-	private int mLeftButtonPressed     = NOT_PRESSED;
-	private int mRightButtonPressed    = NOT_PRESSED;
-	private int mScrollBarPressed      = NOT_PRESSED;
-	private int mKeyboardButtonPressed = NOT_PRESSED;
-	
+
 	/**
      * Constructor
      * @param context
@@ -46,9 +36,6 @@ public class TouchPadView extends SurfaceView implements View.OnTouchListener, S
      */
 	public TouchPadView(Context context) {
 		super(context);
-		
-		// Set up to receive touch events
-		setOnTouchListener(this);
 		
 		// Set up to receive surface events
 		getHolder().addCallback(this);
@@ -93,128 +80,8 @@ public class TouchPadView extends SurfaceView implements View.OnTouchListener, S
 		if (DBG) Log.i(TAG, "+++ SURFACE DESTROYED +++");	
 	}
 	
-	/**
-	 * Called when the view is touched.
-	 */
-	public boolean onTouch(View view, MotionEvent event) {
-		if (DBG) Log.d(TAG, "Pointer count:" + event.getPointerCount());
-
-		RemotaService service = RemotaService.getInstance();
-		int c = event.getPointerCount();
-		float x, y;
-		int id, action, actionMasked, actionId;
-		ArrayList<Integer> idList = new ArrayList<Integer>();
-		String str = "";
-		action = event.getAction();
-		actionMasked = event.getActionMasked();
-		actionId = event.getActionIndex();
-		
-		if (DBG) {
-			Log.d(TAG, 
-					", action:" + action +
-					", actionMasked:" + actionMasked +
-					", actionId:" + actionId
-			);
-		}
-		
-		x = event.getX(actionId);
-		y = event.getY(actionId);
-		id = event.getPointerId(actionId);
-		PointF point = new PointF(x, y);
-
-		if (actionMasked == MotionEvent.ACTION_DOWN || actionMasked == MotionEvent.ACTION_POINTER_1_DOWN) {
-			if (pointFIsInRectF(point, getLeftButtonRectF())) {
-				if (mLeftButtonPressed == NOT_PRESSED) {
-					mLeftButtonPressed = id;
-					service.sendMouseEvent(
-							new MouseEvent(MouseEvent.FLAG_LEFT_DOWN, (int)x, (int)y)
-					);
-				}
-			} else if (pointFIsInRectF(point, getRightButtonRectF())) {
-				if (mRightButtonPressed == NOT_PRESSED) {
-					mRightButtonPressed = id;
-					service.sendMouseEvent(
-							new MouseEvent(MouseEvent.FLAG_RIGHT_DOWN, (int)x, (int)y)
-					);
-				}
-			} else if (pointFIsInRectF(point, getScrollBarRectF())) { 
-				if (mScrollBarPressed == NOT_PRESSED) {
-					mScrollBarPressed = id;
-					service.sendMouseEvent(
-							new MouseEvent(MouseEvent.FLAG_WHELL, (int)x, (int)y)
-					);
-				}
-			} else if (pointFIsInRectF(point, getKeyboardButtonRectF())) { 
-				if (mKeyboardButtonPressed == NOT_PRESSED) {
-					mKeyboardButtonPressed = id;
-				}
-			}
-		} else if (actionMasked == MotionEvent.ACTION_UP || actionMasked == MotionEvent.ACTION_POINTER_1_UP) {
-			if (pointFIsInRectF(point, getLeftButtonRectF())) {
-				if (mLeftButtonPressed == id) {
-					mLeftButtonPressed = NOT_PRESSED;
-					service.sendMouseEvent(
-							new MouseEvent(MouseEvent.FLAG_LEFT_UP, (int)x, (int)y)
-					);
-				}
-			} else if (pointFIsInRectF(point, getRightButtonRectF())) {
-				if (mRightButtonPressed == id) {
-					mRightButtonPressed = NOT_PRESSED;
-					service.sendMouseEvent(
-							new MouseEvent(MouseEvent.FLAG_RIGHT_UP, (int)x, (int)y)
-					);
-				}
-			} else if (pointFIsInRectF(point, getScrollBarRectF())) { 
-				if (mScrollBarPressed == id) {
-					mScrollBarPressed = NOT_PRESSED;
-					service.sendMouseEvent(
-							new MouseEvent(MouseEvent.FLAG_WHELL, (int)x, (int)y)
-					);
-				}
-			} else if (pointFIsInRectF(point, getKeyboardButtonRectF())) { 
-				if (mKeyboardButtonPressed == id) {
-					mKeyboardButtonPressed = NOT_PRESSED;
-				}
-			}
-		}
-
-		for (int i = 0; i < c; i++){
-			x = event.getX(i);
-			y = event.getY(i);
-			id = event.getPointerId(i);
-			idList.add(new Integer(id));
-			point = new PointF(x, y);
-			
-			// Check whether a button is down.
-			if (pointFIsInRectF(point, getLeftButtonRectF())) {
-				str = "left";
-			}
-			else if (pointFIsInRectF(point, getRightButtonRectF())) {
-				str = "right";
-			}
-			else if (pointFIsInRectF(point, getScrollBarRectF())) {
-				str = "scroll";
-			}
-			else if (pointFIsInRectF(point, getKeyboardButtonRectF())) {
-				str = "keyboard";
-			}
-			
-			// Check whether a button up.
-			
-			if (DBG) {
-				Log.d(TAG, 
-						"X" + i + ":" + x +
-						",Y" + i + ":" + y +
-						", id:" + id +
-						"," + str);
-			}
-		}
-		
-		return true;
-	}
-	
 	// Return the left button rectangle.
-	private RectF getLeftButtonRectF() {
+	public RectF getLeftButtonRectF() {
 		RectF rectf = new RectF(
 				0.0f,
 				0.0f,
@@ -226,7 +93,7 @@ public class TouchPadView extends SurfaceView implements View.OnTouchListener, S
 	}
 	
 	// Return the right button rectangle.
-	private RectF getRightButtonRectF() {
+	public RectF getRightButtonRectF() {
 		RectF rectf = new RectF(
 				mCanvasWidth * (0.5f - (SCROLLBAR_WIDTH_RATIO / 2.0f)),
 				0.0f,
@@ -238,7 +105,7 @@ public class TouchPadView extends SurfaceView implements View.OnTouchListener, S
 	}
 	
 	// Return the Scroll rectangle.
-	private RectF getScrollBarRectF() {
+	public RectF getScrollBarRectF() {
 		RectF rectf = new RectF(
 				mCanvasWidth * (1.0f - SCROLLBAR_WIDTH_RATIO),
 				0.0f,
@@ -250,7 +117,7 @@ public class TouchPadView extends SurfaceView implements View.OnTouchListener, S
 	}
 	
 	// Return the keyboard button rectangle.
-	private RectF getKeyboardButtonRectF() {
+	public RectF getKeyboardButtonRectF() {
 		RectF rectf = new RectF(
 				0.0f,
 				mCanvasHeight * (1.0f - KEYBOARD_BUTTON_HEIGHT_RATIO),
@@ -261,8 +128,20 @@ public class TouchPadView extends SurfaceView implements View.OnTouchListener, S
 		return rectf;
 	}
 	
+	// Return the touch pad rectangle.
+	public RectF getTouchPadRectF() {
+		RectF rectf = new RectF(
+				0.0f,
+				mCanvasHeight * BUTTON_HEIGHT_RATIO,
+				mCanvasWidth * (1.0f - SCROLLBAR_WIDTH_RATIO),
+				mCanvasHeight * (1.0f - KEYBOARD_BUTTON_HEIGHT_RATIO)
+		);
+		
+		return rectf;
+	}
+	
 	// Return true if the point is in the rectangle.
-	private static boolean pointFIsInRectF(PointF point, RectF rect) {
+	public static boolean pointFIsInRectF(PointF point, RectF rect) {
 		if (rect.left <= point.x && point.x <= rect.right) {
 			if (rect.top <= point.y && point.y <= rect.bottom) { 
 				return true;
@@ -280,5 +159,7 @@ public class TouchPadView extends SurfaceView implements View.OnTouchListener, S
 		canvas.drawRect(getRightButtonRectF(), paint);
 		canvas.drawRect(getScrollBarRectF(), paint);
 		canvas.drawRect(getKeyboardButtonRectF(), paint);
+		paint.setStyle(Paint.Style.FILL);
+		canvas.drawRect(getTouchPadRectF(), paint);
 	}
 }
