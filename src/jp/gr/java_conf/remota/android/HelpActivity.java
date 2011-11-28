48fb37e9787624f954da72bc942a5cc0a415f162
muroi <test.muroi@gmail.com>
2011-11-28 23:34:13 +0900

    Add HelpActivity

---
+++ src/jp/gr/java_conf/remota/android/HelpActivity.java
@@ -0,0 +1,29 @@
+package jp.gr.java_conf.remota.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+/**
+ * 
+ */
+public class HelpActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		WebView webView = new WebView(this);
+		String html = 
+			"<html><head><body bgcolor=\"black\" text=\"white\">" + 
+			getString(R.string.help_introduction) +
+			getString(R.string.help_requirements) +
+			getString(R.string.help_preparation) +
+			getString(R.string.help_how_to_use) +
+			"</body></head></html>";
+		webView.loadData(html, "text/html", "UTF-8");
+		
+		setContentView(webView);
+		
+		setResult(Activity.RESULT_OK);
+	}
+}
