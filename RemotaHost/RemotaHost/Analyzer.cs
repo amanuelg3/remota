26252e1f161ee70a27e56a9a879b3e0336b10d79
muroi <test.muroi@gmail.com>
2011-12-06 01:10:53 +0900

    Add RemotaHost(for Windows host PC) src files

---
+++ RemotaHost/RemotaHost/Analyzer.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemotaHost
+{
+    public class Analyzer
+    {
+        // Constants
+        public const int TYPE_MOUSE = 0xFFF0;
+        public const int TYPE_KEYBOARD = 0xFFF1;
+        public const int TYPE_UNKNOWN = 0xFFFF;
+        public const int SIZE_MOUSE_DATA = 19;
+        public const int SIZE_KEYBOARD_DATA = 9;
+        public const int EOF = 0xC1;
+
+        public static int getType(byte[] buffer, int bytes)
+        {
+            // First 2 bytes indicate the type of data.
+            if (bytes <= 1)
+            {
+                return TYPE_UNKNOWN;
+            }
+
+            byte[] tmp = {0, 0, buffer[0], buffer[1]};
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(tmp);
+            }
+            int type = BitConverter.ToInt32(tmp, 0);
+
+            return type;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="bytes"></param>
+        /// <returns>MOUSEINPUT</returns>
+        public static int getMouseInput(ref User32.MOUSEINPUT mi, byte[] buffer, int bytes)
+        {
+            // MouseInput data size is SIZE_MOUSE_DATA bytes
+            if (bytes < SIZE_MOUSE_DATA || buffer.Length < SIZE_MOUSE_DATA)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            // MouseInput data has the end of frame byte "EOF"
+            if (buffer[SIZE_MOUSE_DATA-1] != EOF)
+            {
+                throw new ArgumentException("the end frame is not EOF");
+            }
+
+            // 0, 1: type
+            if (getType(buffer, bytes) != TYPE_MOUSE)
+            {
+                throw new ArgumentException("not mouse event data");
+            }
+
+            // 2, 3, 4, 5: flag
+            byte[] buf_flag = { buffer[2], buffer[3], buffer[4], buffer[5] };
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(buf_flag);
+            }
+            int flag = BitConverter.ToInt32(buf_flag, 0);
+
+            // 6, 7, 8, 9: x
+            byte[] buf_x = { buffer[6], buffer[7], buffer[8], buffer[9] };
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(buf_x);
+            }
+            int x = BitConverter.ToInt32(buf_x, 0);
+
+            // 10, 11, 12, 13: y
+            byte[] buf_y = { buffer[10], buffer[11], buffer[12], buffer[13] };
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(buf_y);
+            }
+            int y = BitConverter.ToInt32(buf_y, 0);
+
+            // 14, 15, 16, 17: whell
+            byte[] buf_wheel = { buffer[14], buffer[15], buffer[16], buffer[17] };
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(buf_wheel);
+            }
+            int wheel = BitConverter.ToInt32(buf_wheel, 0);
+
+            mi.dx = x;
+            mi.dy = y;
+            mi.mouseData = wheel;
+            mi.dwFlags = flag;
+
+            return SIZE_MOUSE_DATA;
+        }
+
+        public static int getKeyboardInput(ref ArrayList kiList, byte[] buffer, int bytes)
+        {
+            User32.KEYBDINPUT ki = new User32.KEYBDINPUT();
+
+            // KeyboardInput data size is SIZE_KEYBOARD_DATA bytes
+            if (bytes < SIZE_KEYBOARD_DATA || buffer.Length < SIZE_KEYBOARD_DATA)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            // 0, 1: type
+            if (getType(buffer, bytes) != TYPE_KEYBOARD)
+            {
+                throw new ArgumentException("not keyboard event data");
+            }
+
+            // 2, 3: the number of inputs
+            byte[] buf_nInputs = { buffer[2], buffer[3] };
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(buf_nInputs);
+            }
+            int nInputs = BitConverter.ToUInt16(buf_nInputs, 0);
+
+            // KeyboardInput data has the end of frame byte "EOF"
+            if (buffer[2+2+nInputs*6+1-1] != EOF)
+            {
+                throw new ArgumentException("the end frame is not EOF");
+            }
+
+            int flag;
+            ushort scanCode;
+            for (int i = 0; i < nInputs; i++)
+            {
+                ki = new User32.KEYBDINPUT();
+
+                // 4+i*6, 5+i*6, 6+i*6, 7+i*6: i-th flag
+                byte[] buf_flag = { buffer[4+i*6], buffer[5+i*6], buffer[6+i*6], buffer[7+i*6] };
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(buf_flag);
+                }
+                flag = BitConverter.ToInt32(buf_flag, 0);
+
+                // 8+i*6, 9+i*6: scan code
+                byte[] buf_scanCode = { buffer[8+i*6], buffer[9+i*6] };
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(buf_scanCode);
+                }
+                scanCode = BitConverter.ToUInt16(buf_scanCode, 0);
+
+                ki.dwFlags = flag;
+                ki.wScan = scanCode;
+
+                kiList.Add(ki);
+            }
+
+            return (2 + 2 + nInputs * 6 + 1);
+        }
+    }
+}
---
+++ RemotaHost/RemotaHost/RemotaHostForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace RemotaHost
+{
+    partial class RemotaHostForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RemotaHostForm));
+            this.serialPortListBox = new System.Windows.Forms.ListBox();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.serialPortListLabel = new System.Windows.Forms.Label();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.moveSensitivy = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.wheelSensitivy = new System.Windows.Forms.NumericUpDown();
+            this.contextMainMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyMainIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.moveSensitivy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wheelSensitivy)).BeginInit();
+            this.contextMainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPortListBox
+            // 
+            this.serialPortListBox.FormattingEnabled = true;
+            this.serialPortListBox.ItemHeight = 17;
+            this.serialPortListBox.Location = new System.Drawing.Point(18, 34);
+            this.serialPortListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.serialPortListBox.Name = "serialPortListBox";
+            this.serialPortListBox.Size = new System.Drawing.Size(179, 123);
+            this.serialPortListBox.TabIndex = 0;
+            this.serialPortListBox.SelectedIndexChanged += new System.EventHandler(this.onSelectedSerialPortChanged);
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.onDataReceived);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(207, 34);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(112, 33);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = global::RemotaHost.Properties.Resources.Start;
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.onStartButtonClicked);
+            // 
+            // serialPortListLabel
+            // 
+            this.serialPortListLabel.AutoSize = true;
+            this.serialPortListLabel.Location = new System.Drawing.Point(14, 12);
+            this.serialPortListLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.serialPortListLabel.Name = "serialPortListLabel";
+            this.serialPortListLabel.Size = new System.Drawing.Size(143, 17);
+            this.serialPortListLabel.TabIndex = 2;
+            this.serialPortListLabel.Text = global::RemotaHost.Properties.Resources.SelectComPort;
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Enabled = false;
+            this.logTextBox.Location = new System.Drawing.Point(18, 167);
+            this.logTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logTextBox.Size = new System.Drawing.Size(507, 210);
+            this.logTextBox.TabIndex = 3;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(329, 34);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(112, 33);
+            this.stopButton.TabIndex = 4;
+            this.stopButton.Text = global::RemotaHost.Properties.Resources.Stop;
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.onStopButtonClicked);
+            // 
+            // moveSensitivy
+            // 
+            this.moveSensitivy.Location = new System.Drawing.Point(346, 80);
+            this.moveSensitivy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.moveSensitivy.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.moveSensitivy.Name = "moveSensitivy";
+            this.moveSensitivy.Size = new System.Drawing.Size(180, 24);
+            this.moveSensitivy.TabIndex = 5;
+            this.moveSensitivy.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 84);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = global::RemotaHost.Properties.Resources.MoveSensitivity;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = global::RemotaHost.Properties.Resources.WheelSensitivity;
+            // 
+            // wheelSensitivy
+            // 
+            this.wheelSensitivy.Location = new System.Drawing.Point(346, 123);
+            this.wheelSensitivy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.wheelSensitivy.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.wheelSensitivy.Name = "wheelSensitivy";
+            this.wheelSensitivy.Size = new System.Drawing.Size(180, 24);
+            this.wheelSensitivy.TabIndex = 8;
+            this.wheelSensitivy.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // contextMainMenuStrip
+            // 
+            this.contextMainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SettingsToolStripMenuItem,
+            this.ExitToolStripMenuItem});
+            this.contextMainMenuStrip.Name = "contextMainMenuStrip";
+            this.contextMainMenuStrip.Size = new System.Drawing.Size(153, 82);
+            this.contextMainMenuStrip.Text = global::RemotaHost.Properties.Resources.AppName;
+            this.contextMainMenuStrip.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // SettingsToolStripMenuItem
+            // 
+            this.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem";
+            this.SettingsToolStripMenuItem.Size = new System.Drawing.Size(152, 28);
+            this.SettingsToolStripMenuItem.Text = global::RemotaHost.Properties.Resources.Settings;
+            this.SettingsToolStripMenuItem.Click += new System.EventHandler(this.onSettingMenuSelected);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(152, 28);
+            this.ExitToolStripMenuItem.Text = global::RemotaHost.Properties.Resources.Exit;
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.onExitMenuSelected);
+            // 
+            // notifyMainIcon
+            // 
+            this.notifyMainIcon.ContextMenuStrip = this.contextMainMenuStrip;
+            this.notifyMainIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyMainIcon.Icon")));
+            this.notifyMainIcon.Text = global::RemotaHost.Properties.Resources.AppName;
+            this.notifyMainIcon.Visible = true;
+            this.notifyMainIcon.DoubleClick += new System.EventHandler(this.onNotifyMainDobleClicked);
+            // 
+            // RemotaHostForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(545, 390);
+            this.Controls.Add(this.wheelSensitivy);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.moveSensitivy);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.serialPortListLabel);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.serialPortListBox);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "RemotaHostForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "RemotaHost";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.onMainFormClosing);
+            this.Load += new System.EventHandler(this.RemotaHostForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.moveSensitivy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wheelSensitivy)).EndInit();
+            this.contextMainMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox serialPortListBox;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label serialPortListLabel;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.NumericUpDown moveSensitivy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown wheelSensitivy;
+        private System.Windows.Forms.ContextMenuStrip contextMainMenuStrip;
+        private System.Windows.Forms.NotifyIcon notifyMainIcon;
+        private System.Windows.Forms.ToolStripMenuItem SettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+
+    }
+}
+
