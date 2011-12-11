afa1d3dd4039917b4d8bdb2776914b72dd1ddc25
muroi <test.muroi@gmail.com>
2011-12-12 00:46:39 +0900

    Add help contents

--- src/jp/gr/java_conf/remota/android/HelpActivity.java
+++ src/jp/gr/java_conf/remota/android/HelpActivity.java
@@ -2,6 +2,7 @@ package jp.gr.java_conf.remota.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 /**
@@ -13,14 +14,10 @@ public class HelpActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		WebView webView = new WebView(this);
-		String html = 
-			"<html><head><body bgcolor=\"black\" text=\"white\">" + 
-			getString(R.string.help_introduction) +
-			getString(R.string.help_requirements) +
-			getString(R.string.help_preparations) +
-			getString(R.string.help_how_to_use) +
-			"</body></head></html>";
-		webView.loadData(html, "text/html", "UTF-8");
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setBuiltInZoomControls(true);
+		webSettings.setUseWideViewPort(true);
+		webView.loadUrl(getResources().getString(R.string.help_url));
 		
 		setContentView(webView);
 		
