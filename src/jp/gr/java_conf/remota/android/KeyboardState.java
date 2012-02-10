224d2023c775e063456952f540ec9c93291b410a
muroi <test.muroi@gmail.com>
2012-02-10 22:20:12 +0900

    Add copyright and licence descriptions

--- src/jp/gr/java_conf/remota/android/KeyboardState.java
+++ src/jp/gr/java_conf/remota/android/KeyboardState.java
@@ -1,3 +1,11 @@
+/* Copyright (C) 2011-2012 Test Muroi (test.muroi@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ */
+
 package jp.gr.java_conf.remota.android;
 
 public class KeyboardState {
